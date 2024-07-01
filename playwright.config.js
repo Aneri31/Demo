af30314cb8749aca6b39d47948888454eb9124f8
Aneri31 <aneripatel292@gmail.com>
2024-07-01 15:34:15 +0530

    updated

--- playwright.config.js
+++ playwright.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   testDir: 'tests', // Adjusted relative path
   timeout: 30000,
   use: {
-    headless: false,
+    headless: true,
     viewport: { width: 1280, height: 720 },
     actionTimeout: 10000,
     ignoreHTTPSErrors: true,
